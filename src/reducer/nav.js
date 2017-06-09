93b6eaf5dd4946d247bc72cb22c55773864a0e45
necfol <necfoll@gmail.com>
2017-06-09 16:05:15 +0800

    删除无用代码

--- src/reducer/nav.js
+++ src/reducer/nav.js
@@ -1,29 +1,4 @@
-import { NavigationActions } from 'react-navigation';
 import { AppNavigator } from '../routerConfig.js';
-const homeAction = AppNavigator.router.getActionForPathAndParams('Home');
-const tempNavState = AppNavigator.router.getStateForAction(homeAction);
-const chatAction = AppNavigator.router.getActionForPathAndParams('Chat');
-const initialNavState = AppNavigator.router.getStateForAction(
-  homeAction
-);
-export default (state = initialNavState, action) => {
-  let nextState;
-  switch (action.type) {
-    case 'Home':
-      nextState = AppNavigator.router.getStateForAction(
-        NavigationActions.back(),
-        state
-      );
-      break;
-    case 'Chat':
-      nextState = AppNavigator.router.getStateForAction(
-        NavigationActions.navigate({ routeName: 'Chat' }),
-        state
-      );
-      break;
-    default:
-      nextState = AppNavigator.router.getStateForAction(action, state);
-      break;
-  }
-  return nextState || state;
+export default (state, action) => {
+  return AppNavigator.router.getStateForAction(action, state) || state;
 }
